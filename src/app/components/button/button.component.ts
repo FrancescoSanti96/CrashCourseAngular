4d003335b5dabe68473f5b5db7b469d036880ed2
FrancescoSanti96 <francescosanti123@gmail.com>
2022-10-28 11:27:09 +0200

    component button witch input and output

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+// Input per le props
+// Output ed Evenmitter per il ritorno dell'evento
 
 @Component({
   selector: 'app-button',
@@ -9,8 +11,14 @@ export class ButtonComponent implements OnInit {
   // Input sono come le props in react andare a vedere in button - ButtonComponent.component.html che il compoennbte é richiamato e li vengo passate delle props
   @Input() text: string = '';
   @Input() color: string = '';
+  @Output() btnClick = new EventEmitter();
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  onClick() {
+    this.btnClick.emit();
+    console.log('click');
+  }
 }
