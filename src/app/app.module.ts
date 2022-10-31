c8cc33064fe1804fdbf1d3c92502f5f0b4afe434
FrancescoSanti96 <francescosanti123@gmail.com>
2022-10-31 15:14:22 +0100

    add http request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -10,8 +11,15 @@ import { TaskItemComponent } from './components/task-item/task-item.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, ButtonComponent, TopBarComponent, TaskComponent, TaskItemComponent],
-  imports: [BrowserModule, FontAwesomeModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    TopBarComponent,
+    TaskComponent,
+    TaskItemComponent,
+  ],
+  imports: [BrowserModule, FontAwesomeModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
